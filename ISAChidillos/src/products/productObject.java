583a25c7f657a4cd3a2a23c2dffdb363fe933805
Guillermo Natera <natera27@gmail.com>
2013-04-20 15:01:27 -0500

    Added bluray menu item

--- ISAChidillos/src/products/productObject.java
+++ ISAChidillos/src/products/productObject.java
@@ -1,5 +1,20 @@
 package products;
 
+import java.lang.String;
+
 public class productObject {
 
+	//Variables to Define product Info
+	int modelId;
+	String modelName;
+	String manufature;
+	int productPrice;
+	//Variables to define power consumption
+	int normalConsumption; 
+	int standByConsumption;
+	//Variables to store total consumption based on energy cost, product usage time-stand by time
+	int monthlyCost;
+	int bimestralCost;
+	int semestralCost;
+	int anualCost;		
 }
