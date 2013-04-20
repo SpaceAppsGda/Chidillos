132e70821623d4adc7afab1b3f408e71ff2516b2
Guillermo Natera <natera27@gmail.com>
2013-04-20 18:44:13 -0500

    added metoth to change content on category selection

--- ISAChidillos/src/com/example/isachidillos/MainActivity.java
+++ ISAChidillos/src/com/example/isachidillos/MainActivity.java
@@ -2,9 +2,14 @@ package com.example.isachidillos;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import com.agimind.widget.SlideHolder;
 import products.productObject;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends Activity {
 
@@ -21,4 +26,15 @@ public class MainActivity extends Activity {
 		return true;
 	}
 
+	public void categorySelection(View v)
+	{
+		RelativeLayout dynamicLayout = (RelativeLayout) this.findViewById(R.id.dynamicLayout);
+		LayoutInflater layoutInflater = (LayoutInflater) this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		dynamicLayout.removeAllViews();
+		View newContent = null;
+		newContent = layoutInflater.inflate(R.layout.compare_selection,null);	
+		//newContent.setId(1);
+		dynamicLayout.addView(newContent);
+		
+	}
 }
