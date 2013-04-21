2e1918750b66af635af16e7537956a47c9e20efe
Guillermo Natera <natera27@gmail.com>
2013-04-21 05:06:37 -0500

    natera to master

--- ISAChidillos/gen/com/example/isachidillos/R.java
+++ ISAChidillos/gen/com/example/isachidillos/R.java
@@ -21,18 +21,46 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int blueray=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
+        public static final int amplifier=0x7f020000;
+        public static final int bluray=0x7f020001;
+        public static final int computadoras=0x7f020002;
+        public static final int digitaltvrecorder=0x7f020003;
+        public static final int game=0x7f020004;
+        public static final int ic_launcher=0x7f020005;
+        public static final int impresoras=0x7f020006;
+        public static final int mediaplayers=0x7f020007;
+        public static final int monitor=0x7f020008;
+        public static final int nas=0x7f020009;
+        public static final int switch_=0x7f02000a;
+        public static final int tele=0x7f02000b;
+        public static final int wifi=0x7f02000c;
     }
     public static final class id {
         public static final int BluRay=0x7f080001;
-        public static final int action_settings=0x7f080004;
+        public static final int action_settings=0x7f080012;
+        public static final int button1=0x7f080011;
+        public static final int compare_selection=0x7f080005;
+        public static final int dynamicLayout=0x7f080003;
+        public static final int editText1=0x7f080010;
         public static final int imageView1=0x7f080002;
+        public static final int manufacturerText=0x7f080009;
+        public static final int manufacturer_decrement=0x7f080008;
+        public static final int manufacturer_increment=0x7f08000a;
+        public static final int modelText=0x7f08000d;
+        public static final int model_decrement=0x7f08000c;
+        public static final int model_increment=0x7f08000e;
+        public static final int scrollView1=0x7f080006;
         public static final int slideHolder=0x7f080000;
-        public static final int textView=0x7f080003;
+        public static final int textView=0x7f080004;
+        public static final int textView1=0x7f080007;
+        public static final int textView2=0x7f08000b;
+        public static final int textView3=0x7f08000f;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int compare_selection=0x7f030001;
+        public static final int product_selection=0x7f030002;
+        public static final int results=0x7f030003;
     }
     public static final class menu {
         public static final int main=0x7f070000;
--- ISAChidillos/src/com/example/isachidillos/MainActivity.java
+++ ISAChidillos/src/com/example/isachidillos/MainActivity.java
@@ -1,17 +1,41 @@
 package com.example.isachidillos;
 
-import android.os.Bundle;
-import android.app.Activity;
-import android.view.Menu;
+import java.util.ArrayList;
+
 import com.agimind.widget.SlideHolder;
 import products.productObject;
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.View;
+import android.widget.ExpandableListView;
+import android.widget.RelativeLayout;
+import android.widget.ExpandableListAdapter;
+import android.widget.TextView;
+
+
 
 public class MainActivity extends Activity {
 
+	public ArrayList<String> manufacturers = new ArrayList<String>();
+	public int manufacturerIndex=0;
+	public ArrayList<String> models = new ArrayList<String>();
+	public int modelIndex = 0;
+	public int currentCategory = 0;
+	public String currentManufacturer="";
+	public String currentModel = "";
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		productObject product = new productObject();
+		this.manufacturers.add("Apple");
+		this.manufacturers.add("Dell");
+		this.models.add("T1");
+		this.models.add("T2");
 	}
 
 	@Override
@@ -19,6 +43,84 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+
+	public void categorySelection(View v)
+	{
+		SlideHolder holder = (SlideHolder) this.findViewById(R.id.slideHolder);
+		holder.completeClosing();
+				
+		//Highlight selection
+		v.setBackgroundColor(0x89AB2B);
+		//Change layout to product selection layout
+		RelativeLayout dynamicLayout = (RelativeLayout) this.findViewById(R.id.dynamicLayout);
+		LayoutInflater layoutInflater = (LayoutInflater) this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		dynamicLayout.removeAllViews();
+		View newContent = null;
+		newContent = layoutInflater.inflate(R.layout.product_selection,null);	
+		//newContent.setId(1);
+		dynamicLayout.addView(newContent);
+
+		//Set the current category Id to query http://www.tpcdb.com/list.php?type= categoryid
+		currentCategory = Integer.parseInt(v.getTag().toString());
+		setManufacturerText();
+		
+		//Obtain Manufacturers Array from that category
+		
+		//Populate the product selection manufacturers text
+		
+		//Set current manufacture to the first object in manufacturers array
+		
+		//Obtain Models Array from http://www.tpcdb.com/list.php?type= categoryid & query=currentManufacture
+	}
+	
+	public void setManufacturerText()
+	{
+		TextView text = (TextView) this.findViewById(R.id.manufacturerText);
+		text.setText(currentManufacturer);
+	}
+	
+	public void incrementCurrentManufacturer(View v)
+	{
+		//add a validation max value = arraysize-1
+		if(manufacturerIndex != (this.manufacturers.size()-1))
+			manufacturerIndex++;
+		currentManufacturer = this.manufacturers.get(manufacturerIndex);
+		setManufacturerText();
+		//
+	}
+	
+	public void decrementCurrentManufacturer(View v)
+	{
+		//Add a validation min value = 0
+		if(manufacturerIndex != 0)
+			manufacturerIndex--;
+		currentManufacturer = this.manufacturers.get(manufacturerIndex);
+		setManufacturerText();
+	}
+	
+	public void setModelText()
+	{
+		TextView text = (TextView) this.findViewById(R.id.modelText);
+		text.setText(currentModel);
+	}
+	
+	public void incrementCurrentModel(View v)
+	{
+		//add a validation max value = arraysize-1
+		if(modelIndex != (this.models.size()-1) )
+			modelIndex++;
+		currentModel = this.models.get(modelIndex);
+		setModelText();
+	}
+	
+	public void decrementCurrentModel(View v)
+	{
+		//Validation min value = 0
+		if(modelIndex!=0)
+			modelIndex--;
+		currentModel = this.models.get(modelIndex);
+		setModelText();
 	}
 
 }
